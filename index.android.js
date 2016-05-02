188525c151562aae1b46f02fa4fef92ae85a3dcf
Adam Dunlop <adamblairdunlop@gmail.com>
2016-05-01 18:06:39 -0700

    added new gen files

--- index.android.js
+++ index.android.js
@@ -16,7 +16,7 @@ class Snappshot extends Component {
     return (  
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Snappshot Edit
+          Snappshot Edit again
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
