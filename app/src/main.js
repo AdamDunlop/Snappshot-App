e90ad7b377e9d38f49f5ce38d1ca482b3231b7f5
Andrea Cabral <andrea.cabralb@gmail.com>
2016-05-04 11:41:23 -0700

    misc + cleaned code

--- app/src/main.js
+++ app/src/main.js
@@ -1,12 +1,3 @@
-// import Greeting from './models/Greeting';
-// import DateTime from './models/DateTime';
- 
-// var h1 = document.querySelector('h1');
-// h1.textContent = new Greeting();
- 
-// var h2 = document.querySelector('h2');
-// h2.textContent = new DateTime();
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
